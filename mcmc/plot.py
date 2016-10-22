b14664bb33901b2e405e5c6681509509e8db080f
Xinyang Li <soybeanyoung@gmail.com>
2016-10-22 17:59:17 -0400

    update plot.py

--- mcmc/plot.py
+++ mcmc/plot.py
@@ -6,13 +6,12 @@ import matplotlib.pyplot as plt
 import networkx.drawing
 
 class plot_graph():
-    # would like to share this property for all possible functions
-    # not sure if it's correct to use like this
-    plt.figure(figsize=(5,5))
 
     def __init__(self):
         pass
     # plot the graph specified in the bracket
     def plot_this_graph(self, pos, G):
+        # would like to share this property for all possible functions
+        plt.figure(figsize=(5,5))
         nx.draw_networkx(G, pos, node_size=20)
         plt.show()
