bdc2bb3c9d3aec4d4b05f811938f1ed96cc22671
Xinyang Li <soybeanyoung@gmail.com>
2016-10-22 19:42:28 -0400

    for cov

--- mcmc/plot.py
+++ mcmc/plot.py
@@ -2,6 +2,8 @@
 # currently only contain the function to plot specified graphs
 import networkx as nx
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import networkx.drawing
 
@@ -13,5 +15,6 @@ class plot_graph():
     def plot_this_graph(self, pos, G):
         # would like to share this property for all possible functions
         plt.figure(figsize=(5,5))
+        fig = plt.figure()
         nx.draw_networkx(G, pos, node_size=20)
-        plt.show()
+        fig.savefig('output.png')
